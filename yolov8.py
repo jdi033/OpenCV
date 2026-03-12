804b65273347472200c4d9acdcf76f1e74c69452
jdi <1754105726@qq.com>
2026-03-12 22:16:02 +0800

    2026.3.12

--- yolov8.py
+++ yolov8.py
@@ -39,6 +39,7 @@ class Bottleneck(nn.Module):
     def __init__(self, c1, c2, k=(3,3), s=1, p=None, add=True, e=0.5):
         super().__init__()
         #中间层通道数，使用c2的原因是统一yolov8不同模型l,s,m之间的模型尺寸的缩放
+        #在改动模型缩放时，通常改变配置文件的宽度因子从而改变c2的大小，Bottleneck通过c2*e保证了无论网络怎么变，内部结构的比例不变
         c = int(c2*e)
         #Bottleneck使用两层卷积，所以分别得到 k[0]和 k[1]作为核大小，(k[i]元素可能是整数，也可能是元组)
         self.conv1 = Conv(c1, c, k[0], s, p)
@@ -75,7 +76,8 @@ class C2f(nn.Module):
 class SPPF(nn.Module):
     def __init__(self, c1, c2, k=5):
         super().__init__()
-        #为什么使用c1//2，不明白
+        #如果直接使用c1作为输入通道数，在经过3次最大池化层后，最后一个卷积的输入通道会是4*c1，引发输入通道和参数计算量负载
+        #所以将c1//2特征减半，保留原来特征的同事，降低计算负载
         c_ = c1//2
         self.conv1 = nn.Conv2d(c1, c_, 1, 1)
         #第二个卷积，会接收第一个卷积的输出，3个MaxPool2d的输出
@@ -88,7 +90,7 @@ class SPPF(nn.Module):
         y = self.conv1(x)
         y1 = self.m(y)
         y2 = self.m(y1)
-        return self.cv2(torch.cat((y, y1, y2, self.m(y2)), 1))
+        return self.conv2(torch.cat((y, y1, y2, self.m(y2)), 1))
 
 #DFL
 class DFL(nn.Module):
@@ -105,7 +107,7 @@ class DFL(nn.Module):
 
     def forward(self, x):
         #b为批次大小，c为4*距离维度数(即总共需要预测的维度数)，a为该特征图的总检测点数，[10,64,6400]
-        b, c, a = x.shape()
+        b, c, a = x.shape
         #对x调整形状: [10,64,6400]->[10,4,16,6400]->[10,16,4,6400]
         x_reshape = x.view(b, 4, self.c1, a).permute(0, 2, 1, 3)
         #对维度1使用softmax函数，使得对于每一个位置，16个距离维度的概率总和为1
@@ -128,8 +130,10 @@ class Detect(nn.Module):
         self.no = nc + 4*self.reg_max
         self.stride = torch.zeros(len(ch))
 
-        #分类和回归中间通道数，不明白计算逻辑
+        #分类和回归中间通道数
+        #下限兜底 16 和 4*reg_max (即 64)，确保回归分支在提取特征时有足够的维度去表达 64 个概率分布；同时参考浅层特征图的宽度 ch[0]//4
         c2 = max(16, ch[0]//4, 4*self.reg_max)
+        #参考浅层特征图宽度 ch[0]//4，但如果类别数 nc 极大，用 min(self.nc, 100) 来防止分类分支的中间层变得过度臃肿
         c3 = max(ch[0]//4, min(self.nc, 100))
 
         #分类分支，for x in ch：每个特征图，使用3个卷积，最后通道数为分类个数
@@ -156,29 +160,56 @@ class Detect(nn.Module):
         shape = x[0].shape()
 
         #分类与回归分支，每个特征图经过卷积后的结果
+        #y_cls: [[10,80,80,80],[10,80,60,60],[10,80,40,40]]
         y_cls=[]
         y_reg=[]
         for i in range(self.nl):
-            y_cls.append(self.cv2[i](x[i]))
-            y_reg.append(self.cv3[i](x[i]))
+            #将特征图展平，cls_out:[10,80,6400],reg_out:[10,64,6400]
+            cls_out = self.cv2[i](x[i])
+            reg_out = self.cv3[i](x[i])
+            y_cls.append(cls_out.view(shape[0], self.nc, -1))
+            y_reg.append(reg_out.view(shape[0], self.reg_output_dim, -1))
 
         #cls_concatenated:[10, 80, 6400+1600+400], reg_concatenated:[10, 64, 6400+1600+400]
-        cls_concatenated = torch.cat(y_cls, 1)
-        reg_concatenated = torch.cat(y_reg, 1)
-        #pred:[10, 144, 6400+1600+400]
-        pred = torch.cat((cls_concatenated, reg_concatenated), 1)
+        cls_concatenated = torch.cat(y_cls, 2)
+        reg_concatenated = torch.cat(y_reg, 2)
+
 
         #训练阶段
         if self.training:
+            # pred:[10, 144, 6400+1600+400]
+            pred = torch.cat((cls_concatenated, reg_concatenated), 1)
             return pred
         #推理阶段
         else:
-            # cls_results:[10, 80, 6400+1600+400], reg_to_decode:[10, 64, 6400+1600+400]
-            reg_to_decode, cls_results = pred.split((self.reg_output_dim, self.nc), 1)
-            #在维度1上，对80个类别使用softmax，得到分类标签
-            cls_scores = cls_results.softmax(1)
+            #在yolo中，多类别分类不能使用softmax，因为softmax只能用于类别之间不冲突(概率之和为1)，yolo可以预测出多个类别，是将多分类独立成多个二分类任务
+            #使用 Sigmoid 激活函数，将每个独立通道的 Logit 压缩到[0,1]之间
+            cls_scores = cls_concatenated.sigmoid()
             #得到4个位置的距离，[10,4,6400+1600+400]
-            reg_results = self.dfl(reg_to_decode)
+            reg_results = self.dfl(reg_concatenated)
             #[10,84,6400+1600+400]
             out = torch.cat((cls_scores, reg_results), 1)
             return out
+
+#拼接各模块
+class YOLOv8(nn.Module):
+    def __init__(self, nc=2):
+        super().__init__()
+        self.nc = nc
+
+        self.stem = Conv(3, 16, k=3, s=2)
+
+        self.conv2 = Conv(16, 32, k=3, s=2)
+        self.c2f2 = C2f(32,32,n=1,add=True)
+
+        self.conv3 = Conv(32, 64, k=3, s=2)
+        self.c2f3 = C2f(64,64,n=2,add=True)
+
+        self.conv4 = Conv(64, 128, k=3, s=2)
+        self.c2f4 = C2f(128,128,n=2,add=True)
+
+        self.conv5 = Conv(128, 256, k=3, s=2)
+        self.c2f5 = C2f(256,256,n=2,add=True)
+        self.sppf = SPPF(256,256,k=5)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
